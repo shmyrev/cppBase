37b9e016a3ebb8e9c96fc22b31e73f3498e3e73e
shmyrev <sh.diman@gmail.com>
2020-11-12 20:00:51 +0400

    Shorthand operators that have the capability of performing an operation and an assignment at the same time.

---
+++ assignment_v1-0.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Assignment operators: \n"
+	          << "x += 4 - equivalent to x = x + 4 \n"
+	          << "x -= 5 - equivalent to x = x - 5 \n"
+	          << "x *= 3 - equivalent to x = x * 3 \n"
+		  << "x /= 2 - equivalent to x = x / 2 \n"
+	          << "x %= 4 - equivalent to x = x % 4 \n";
+	return 0;
+}
