d7e5509979a1de6a82f2b58851ff38fe5f618be3
shmyrev <sh.diman@gmail.com>
2020-11-12 10:51:04 +0400

    Supports these arithmetic operators

---
+++ arithmetic_v1-0.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+	int a, b;
+	int add = a + b; // Additation
+	int sub = a - b; // Subtraction
+	int mul = a * b; // Multiplication
+	int div = a / b; // Division
+	int mod = a % b; // Modulus
+
+	std::cout << "Additation: a + b \n";
+	std::cout << "Subtraction: a - b \n";
+	std::cout << "Multiplication: a * b \n";
+	std::cout << "Division: a / b \n";
+	std::cout << "Modulus: a /%/ b \n";
+
+	return 0;
+}
