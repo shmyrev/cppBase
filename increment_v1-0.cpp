2243ae5ab779eefb4eced0776e490be74f872d1d
shmyrev <sh.diman@gmail.com>
2020-11-12 20:05:29 +0400

    The increment operator has two forms, prefix and postfix.

---
+++ increment_v1-0.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+	std::cout << "++x - Prefix increments the value, and then proceeds with the expression. \n"
+		  << "x++ - Postfix evaluates the expression and then performs the incrementing. \n";
+	return 0;
+}
